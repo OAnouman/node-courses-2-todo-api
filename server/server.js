f5264db5f6c50241d53af185babb47f4b082d5d6
Martial Anouman <manouman@live.fr>
2018-03-20 18:07:29 +0000

    Add post todo route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,49 @@
+//#region Imports
+
+// Globals imports
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Local imports
+
 const { mongoose } = require('./db/mongoose');
 
 const { Todo } = require('./models/Todo');
 
 const { User } = require('./models/User');
+
+//#endregion
+
+let port = process.PORT || 3000;
+
+//#region Middleware
+
+app.use(bodyParser.json());
+
+//#endregion
+
+
+
+//#region API routes
+
+app.post('/todos', (req, res, next) => {
+
+    let todo = new Todo({
+
+            text: req.body.text,
+
+        })
+        .save()
+        .then(doc => res.status(200).send(doc))
+        .catch(e => res.status(400).send(`Unable to create todo. Error details : ${e}`));
+
+})
+
+//#endregion
+
+
+app.listen(port, () => console.log(`Server up and running on port ${port}...`));
